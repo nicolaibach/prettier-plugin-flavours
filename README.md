ce76002b03aa8b8967ce8b8dbed39cd58ff8f9e4
Nicolai Bach <mail@nicolaibach.de>
2020-03-27 09:33:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# prettier-plugin-flavours
+
+Adds a bunch of file types to prettier
+
+## Resources
+
+- [Visual Studio Code Language Identifiers](https://code.visualstudio.com/docs/languages/identifiers)
+
+## License
+
+This project is [MIT](https://github.com/nicolaibach/prettier-plugin-flavours/blob/master/LICENSE) licensed.
---
+++ index.js
@@ -0,0 +1,20 @@
+const languages = [
+  {
+    extensions: ['.liquid'],
+    name: 'Liquid',
+    parsers: ['html'],
+    linguistLanguageId: 204,
+    vscodeLanguageIds: ['liquid']
+  },
+  {
+    extensions: ['.jxa'],
+    name: 'JavaScript for Automation (JXA)',
+    parsers: ['javascript'],
+    // linguistLanguageId: ?,
+    vscodeLanguageIds: ['jxa']
+  }
+];
+
+module.exports = {
+  languages
+};
